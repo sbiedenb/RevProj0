df3f519511f5232fde7011d668bbe65985ae3d33
Steven Biedenbach <sbiedenbach@ggc.edu>
2018-06-19 12:53:24 -0400

    Integration

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def main(argv):
 		sys.exit()
 
 	#Calculate the final weights used to compare individuals
-	print(weightValues)
+	#print(weightValues)
 	finalWeights=calcWeight(weightValues)
 
 	#Inserting final weights into the original json dictionary
@@ -63,10 +63,15 @@ def main(argv):
 		weightValues[n]=finalWeights[i]
 		i+=1
 
-	print(weightValues)
+	#print(weightValues)
+
+	persons = readGradesFromFile('./grades.json')
+	conversion = convertData(persons)
+	print(conversion)
 
 	#TEST VALUES
-	finalScore={'John':40,'Mary':20,'Sam':80,'Bob':50}
+	finalScore=candidateScores(weightValues,conversion)
+	#{'John':40,'Mary':20,'Sam':80,'Bob':50}
 
 	#Sort the final list of candidates by overall scores
 	#returns the desired number of top candidates
@@ -101,6 +106,47 @@ def calcWeight(weights):
 	for n in range(0,len(rows)):
 		rows[n]=round(rows[n]/len(rows),2)
 	return rows
+
+#=======================#
+def readGradesFromFile(fileName):
+
+    persons = {}
+
+    with open(fileName) as f:
+        persons = json.load(f)
+
+    return persons
+
+#=======================#
+def convertData(people):
+
+    for person, skills in people.items():
+
+        for technology, score in skills.items():
+                
+            if score >= 95:
+                skills[technology] = 8
+            elif score < 95 and score >= 90:
+                skills[technology] = 7
+            elif score < 90 and score >= 85:
+                skills[technology] = 6
+            elif score < 85 and score >= 80:                     
+                skills[technology] = 5
+            elif score < 80 and score >= 75:                     
+                skills[technology] = 4
+            elif score < 75 and score >= 70:                     
+                skills[technology] = 3
+            elif score < 70 and score >= 65:
+                skills[technology] = 2
+            elif score < 65 and score >= 60:
+                skills[technology] = 1
+            else:
+                skills[technology] = 0
+
+    return people 
+#=======================#
+def candidateScores(catWeights, personWeights):
+	return
 
 #=======================#
 if __name__=='__main__':
